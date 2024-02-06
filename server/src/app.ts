a4c5c80870cc9f2c237b96f732178617e294201a
kkpagaev <kkpagaev@gmail.com>
2024-02-06 02:56:42 +0200

    refactor trpc

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,6 @@ import Fastify from "fastify";
 
 import * as path from "path";
 import { fastifyTRPCPlugin } from "./core/trpc";
-import { FastifyTRPCPluginOptions } from "@trpc/server/adapters/fastify";
-import { createContext } from "./core/context";
-import { AppRouter, createAppRouter } from "./app.router";
-import { ZodTypeProvider } from "fastify-type-provider-zod";
 import fastifyAutoload from "@fastify/autoload";
 
 export async function build() {
@@ -16,19 +12,8 @@ export async function build() {
   await f.register(fastifyAutoload, {
     dir: path.resolve(__dirname, "plugins"),
   });
-
-  const appRouter = await createAppRouter(f.withTypeProvider<ZodTypeProvider>());
-
   await f.register(fastifyTRPCPlugin, {
     prefix: "/trpc",
-    useWSS: true,
-    trpcOptions: {
-      router: appRouter,
-      createContext,
-      onError({ path, error }) {
-        console.error(`Error in tRPC handler on path '${path}':`, error);
-      },
-    } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"],
   });
 
   return f;
--- server/src/core/trpc.ts
+++ server/src/core/trpc.ts
@@ -5,6 +5,9 @@ import {
 } from "@trpc/server/adapters/fastify";
 import { WSSHandlerOptions, applyWSSHandler } from "@trpc/server/adapters/ws";
 import { FastifyInstance } from "fastify";
+import { AppRouter, createAppRouter } from "../app.router";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { createContext } from "./context";
 
 declare module "fastify" {
   interface FastifyInstance {
@@ -17,10 +20,9 @@ export const t = initTRPC.create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-export function fastifyTRPCPlugin<TRouter extends AnyTRPCRouter>(
+export async function fastifyTRPCPlugin<TRouter extends AnyTRPCRouter>(
   fastify: FastifyInstance,
-  opts: FastifyTRPCPluginOptions<TRouter>,
-  done: (err?: Error) => void,
+  opts: { prefix?: string; useWSS?: boolean } = {},
 ) {
   fastify.removeContentTypeParser("application/json");
   fastify.addContentTypeParser(
@@ -37,6 +39,16 @@ export function fastifyTRPCPlugin<TRouter extends AnyTRPCRouter>(
   fastify.decorate("publicProcedure", publicProcedure);
 
   let prefix = opts.prefix ?? "";
+
+  const appRouter = await createAppRouter(fastify.withTypeProvider<ZodTypeProvider>());
+
+  const trpcOptions = {
+    router: appRouter,
+    createContext,
+    onError({ path, error }) {
+      console.error(`Error in tRPC handler on path '${path}':`, error);
+    },
+  } satisfies FastifyTRPCPluginOptions<AppRouter>["trpcOptions"];
 
   // https://github.com/fastify/fastify-plugin/blob/fe079bef6557a83794bf437e14b9b9edb8a74104/plugin.js#L11
   // @ts-expect-error property 'default' does not exists on type ...
@@ -63,18 +75,15 @@ export function fastifyTRPCPlugin<TRouter extends AnyTRPCRouter>(
     const path = handleServerValidationPath(
       (req.params as any).path.toString(),
     );
-    console.log(path);
 
-    await fastifyRequestHandler({ ...opts.trpcOptions, req, res, path });
+    await fastifyRequestHandler({ ...trpcOptions, req, res, path });
   });
 
   if (opts.useWSS) {
     applyWSSHandler<TRouter>({
-      ...(opts.trpcOptions as unknown as WSSHandlerOptions<TRouter>),
+      ...(trpcOptions as unknown as WSSHandlerOptions<TRouter>),
       wss: fastify.websocketServer,
     });
     fastify.get(prefix ?? "/", { websocket: true }, () => {});
   }
-
-  done();
 }
