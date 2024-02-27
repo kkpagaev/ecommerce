86fd849e7e81303efa563e0a9c2144f37c0c11e5
kkpagaev <kkpagaev@gmail.com>
2024-02-27 12:40:15 +0200

    auth

---
+++ server/src/core/auth/auth.ts
@@ -0,0 +1,35 @@
+import * as jwt from "jsonwebtoken";
+
+export interface JwtPayload extends jwt.JwtPayload {
+  userId: number;
+}
+
+export class JwtService {
+  constructor(private readonly secret: string) {}
+
+  sign(payload: JwtPayload): string {
+    return jwt.sign(payload, this.secret);
+  }
+
+  verify(token: string): JwtPayload {
+    return jwt.verify(token, this.secret) as JwtPayload;
+  }
+
+  parseAndVerify(header: string): JwtPayload | undefined {
+    const token = this.parseBearerToken(header);
+
+    if (token) {
+      return this.verify(token);
+    }
+    else {
+      return undefined;
+    }
+  }
+
+  parseBearerToken(jwtHeader: string): string | undefined {
+    const bearer = jwtHeader.split(" ");
+    const token = bearer[1];
+
+    return token;
+  }
+}
