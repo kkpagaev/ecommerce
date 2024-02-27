d5f1575d5fc51c775939cb3b853f044392935e10
kkpagaev <kkpagaev@gmail.com>
2024-02-27 09:29:54 +0200

    filterUpsertEntries util fn

---
+++ server/test/plugins/core/utils.spec.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from "vitest";
+import { filterUpsertEntries } from "../../../src/core/utils";
+
+describe("utils", () => {
+  describe("filterUpsertEntries", () => {
+    it("should filter entries that not in first argument as to delete", () => {
+      const res = filterUpsertEntries([{
+        id: 1,
+        value: "any",
+      }], [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+      ]);
+
+      expect(res.toDelete).toEqual([2]);
+      expect(res.toUpsert).toEqual([{
+        id: 1,
+        value: "any",
+      }]);
+    });
+
+    it("should add entries without id to toUpsert", () => {
+      const res = filterUpsertEntries<{ id: number; value: string }>([
+        {
+          value: "any",
+        },
+      ], []);
+
+      expect(res.toDelete).toEqual([]);
+      expect(res.toUpsert).toEqual([
+        {
+          value: "any",
+        },
+      ]);
+    });
+
+    it("should toDelete all entries if input is empty", () => {
+      const res = filterUpsertEntries([], [
+        {
+          id: 1,
+          value: "any",
+        },
+        {
+          id: 2,
+          value: "any",
+        },
+      ]);
+
+      expect(res.toUpsert).toEqual([]);
+      expect(res.toDelete).toEqual([1, 2]);
+    });
+  });
+});
