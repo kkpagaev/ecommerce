11ceeb60cb1b1ae3b6ba9ae17c341aaec0f91902
kkpagaev <kkpagaev@gmail.com>
2024-03-08 14:15:17 +0200

    attribute to js

--- packages/catalog/src/queries/attribute.types.ts
+++ packages/catalog/src/queries/attribute.types.ts
@@ -1,4 +1,67 @@
-/** Types generated for queries found in "src/attribute.ts" */
+/** Types generated for queries found in "src/attribute.js" */
+
+/** 'AttributeDeletQuery' parameters type */
+export interface IAttributeDeletQueryParams {
+  id: number;
+}
+
+/** 'AttributeDeletQuery' return type */
+export type IAttributeDeletQueryResult = void;
+
+/** 'AttributeDeletQuery' query type */
+export interface IAttributeDeletQueryQuery {
+  params: IAttributeDeletQueryParams;
+  result: IAttributeDeletQueryResult;
+}
+
+/** 'AttributeDescriptionDeleteQuery' parameters type */
+export interface IAttributeDescriptionDeleteQueryParams {
+  attributeId: number;
+  languageId: number;
+}
+
+/** 'AttributeDescriptionDeleteQuery' return type */
+export type IAttributeDescriptionDeleteQueryResult = void;
+
+/** 'AttributeDescriptionDeleteQuery' query type */
+export interface IAttributeDescriptionDeleteQueryQuery {
+  params: IAttributeDescriptionDeleteQueryParams;
+  result: IAttributeDescriptionDeleteQueryResult;
+}
+
+/** 'AttributeCreateQuery' parameters type */
+export interface IAttributeCreateQueryParams {
+  attributeGroupId: number;
+}
+
+/** 'AttributeCreateQuery' return type */
+export interface IAttributeCreateQueryResult {
+  id: number;
+}
+
+/** 'AttributeCreateQuery' query type */
+export interface IAttributeCreateQueryQuery {
+  params: IAttributeCreateQueryParams;
+  result: IAttributeCreateQueryResult;
+}
+
+/** 'AttributeDescriptionUpsertQuery' parameters type */
+export interface IAttributeDescriptionUpsertQueryParams {
+  values: readonly ({
+    attributeId: number,
+    languageId: number,
+    name: string
+  })[];
+}
+
+/** 'AttributeDescriptionUpsertQuery' return type */
+export type IAttributeDescriptionUpsertQueryResult = void;
+
+/** 'AttributeDescriptionUpsertQuery' query type */
+export interface IAttributeDescriptionUpsertQueryQuery {
+  params: IAttributeDescriptionUpsertQueryParams;
+  result: IAttributeDescriptionUpsertQueryResult;
+}
 
 /** 'AttributeFindOneQuery' parameters type */
 export interface IAttributeFindOneQueryParams {
@@ -35,65 +98,3 @@ export interface IAttributeDescriptionListQueryQuery {
   result: IAttributeDescriptionListQueryResult;
 }
 
-/** 'AttributeDescriptionDeleteQuery' parameters type */
-export interface IAttributeDescriptionDeleteQueryParams {
-  attributeId: number;
-  languageId: number;
-}
-
-/** 'AttributeDescriptionDeleteQuery' return type */
-export type IAttributeDescriptionDeleteQueryResult = void;
-
-/** 'AttributeDescriptionDeleteQuery' query type */
-export interface IAttributeDescriptionDeleteQueryQuery {
-  params: IAttributeDescriptionDeleteQueryParams;
-  result: IAttributeDescriptionDeleteQueryResult;
-}
-
-/** 'AttributeDeletQuery' parameters type */
-export interface IAttributeDeletQueryParams {
-  id: number;
-}
-
-/** 'AttributeDeletQuery' return type */
-export type IAttributeDeletQueryResult = void;
-
-/** 'AttributeDeletQuery' query type */
-export interface IAttributeDeletQueryQuery {
-  params: IAttributeDeletQueryParams;
-  result: IAttributeDeletQueryResult;
-}
-
-/** 'AttributeCreateQuery' parameters type */
-export interface IAttributeCreateQueryParams {
-  attributeGroupId: number;
-}
-
-/** 'AttributeCreateQuery' return type */
-export interface IAttributeCreateQueryResult {
-  id: number;
-}
-
-/** 'AttributeCreateQuery' query type */
-export interface IAttributeCreateQueryQuery {
-  params: IAttributeCreateQueryParams;
-  result: IAttributeCreateQueryResult;
-}
-
-/** 'AttributeDescriptionUpsertQuery' parameters type */
-export interface IAttributeDescriptionUpsertQueryParams {
-  values: readonly {
-    attributeId: number;
-    languageId: number;
-    name: string;
-  }[];
-}
-
-/** 'AttributeDescriptionUpsertQuery' return type */
-export type IAttributeDescriptionUpsertQueryResult = void;
-
-/** 'AttributeDescriptionUpsertQuery' query type */
-export interface IAttributeDescriptionUpsertQueryQuery {
-  params: IAttributeDescriptionUpsertQueryParams;
-  result: IAttributeDescriptionUpsertQueryResult;
-}
