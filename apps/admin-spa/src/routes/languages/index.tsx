7373857188e0005b6fe932ff4aac210dd574595f
kkpagaev <kkpagaev@gmail.com>
2024-05-04 15:41:15 +0300

    languages table

--- apps/admin-spa/src/routes/languages/index.tsx
+++ apps/admin-spa/src/routes/languages/index.tsx
@@ -1,21 +1,100 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { api } from "../../utils/trpc";
+import { trpc } from "../../utils/trpc";
+import { DataTable } from "../../components/data-table";
+
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { AdminOutputs } from "@/utils/trpc";
+
+type Language = AdminOutputs["language"]["list"][0];
 
 export const Route = createFileRoute("/languages/")({
-  loader: () => {
-    return api.admin.language.list.query();
-  },
+  beforeLoad: () => ({ getTitle: () => "Languages" }),
   component: Index,
 });
 
+const columns: ColumnDef<Language>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    enableSorting: true,
+    cell: ({ row }) => <div>{row.getValue("id")}</div>,
+  },
+  {
+    accessorKey: "name",
+    enableSorting: true,
+    cell: ({ row }) => {
+      return <div className="capitalize">{row.getValue("name")}</div>;
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: () => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <DotsHorizontalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
+
 function Index() {
-  const data = Route.useLoaderData();
+  const data = trpc.admin.language.list.useQuery();
+  console.log(data.data);
 
   return (
     <div className="container mx-auto py-10">
-      {data.map((lang) => (
-        <div key={lang.id}>{lang.name}</div>
-      ))}
+      <DataTable
+        data={
+          data.data ? { data: data.data, count: data.data.length } : undefined
+        }
+        columns={columns}
+        isLoading={data.isLoading}
+      />
     </div>
   );
 }
