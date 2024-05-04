4d7b29b23c08820d9f5bac5b86f642d4cad60d77
kkpagaev <kkpagaev@gmail.com>
2024-05-04 15:34:20 +0300

    breadcrumbs

--- apps/admin-spa/src/components/layout.tsx
+++ apps/admin-spa/src/components/layout.tsx
@@ -1,8 +1,9 @@
 import { Button } from "./ui/button";
 import { Bell, Home, Menu, Package2, Users } from "lucide-react";
-import { Link, useRouter } from "@tanstack/react-router";
+import { Link, useMatches, useRouter } from "@tanstack/react-router";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { SignIn, SignedIn, UserButton, useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
 
 const pathes: Array<
   { name: string; icon: React.ReactNode } & Parameters<typeof Link>[0]
@@ -40,8 +41,59 @@ function NavBar() {
   );
 }
 
+function Breadcrumbs() {
+  const router = useRouter();
+  const breadcrumbs = router.state.matches.map((match) => {
+    const { routeContext } = match;
+    return {
+      title: routeContext.getTitle?.(),
+      path: match.pathname,
+    };
+  });
+
+  return breadcrumbs.map((breadcrumb, i) => {
+    if (i === breadcrumbs.length - 1) {
+      return (
+        <div key={i} className="text-sm font-medium">
+          {breadcrumb.title}
+        </div>
+      );
+    }
+    return (
+      <Link to={breadcrumb.path} key={i} className="text-sm font-medium">
+        {breadcrumb.title}
+      </Link>
+    );
+  });
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   const { isSignedIn } = useAuth();
+
+  const matches = useMatches();
+
+  useEffect(() => {
+    // Set document title based on lowest matching route with a title
+    const breadcrumbPromises = [...matches]
+      .reverse()
+      .map((match, index) => {
+        if (!("getTitle" in match.routeContext)) {
+          if (index === 0 && import.meta.env.DEV) {
+            // eslint-disable-next-line no-console
+            console.warn("no getTitle", match.pathname, match);
+          }
+          return undefined;
+        }
+        const { routeContext } = match;
+        return routeContext.getTitle?.();
+      })
+      .filter(Boolean);
+
+    void Promise.all(breadcrumbPromises).then((titles) => {
+      document.title = titles.join(" · ");
+      return titles;
+    });
+  }, [matches]);
 
   if (!isSignedIn) {
     return (
@@ -89,7 +141,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
               <NavBar />
             </SheetContent>
           </Sheet>
-          <div className="w-full flex-1"></div>
+          <div className="w-full flex-1 text-center">
+            <Breadcrumbs />
+          </div>
           <SignedIn>
             <UserButton />
           </SignedIn>
--- apps/admin-spa/src/routes/__root.tsx
+++ apps/admin-spa/src/routes/__root.tsx
@@ -1,45 +1,14 @@
-import {
-  createRootRoute,
-  Outlet,
-  useMatches,
-  useRouter,
-} from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Layout } from "../components/layout";
-import { useEffect } from "react";
 
 declare module "@tanstack/react-router" {
   interface RouteContext {
-    getTitle?: () => string | Promise<string>;
+    getTitle?: () => string;
   }
 }
 
 function Root() {
-  const matches = useMatches();
-
-  useEffect(() => {
-    // Set document title based on lowest matching route with a title
-    const breadcrumbPromises = [...matches]
-      .reverse()
-      .map((match, index) => {
-        if (!("getTitle" in match.routeContext)) {
-          if (index === 0 && import.meta.env.DEV) {
-            // eslint-disable-next-line no-console
-            console.warn("no getTitle", match.pathname, match);
-          }
-          return undefined;
-        }
-        const { routeContext } = match;
-        return routeContext.getTitle?.();
-      })
-      .filter(Boolean);
-
-    void Promise.all(breadcrumbPromises).then((titles) => {
-      document.title = titles.join(" · ");
-      return titles;
-    });
-  }, [matches]);
-
   return (
     <>
       <Layout>
