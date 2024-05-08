2e407030240584fe7161edf4e7e1f49d0b854a52
kkpagaev <kkpagaev@gmail.com>
2024-05-08 16:48:58 +0300

    set galery images to product form

--- apps/admin-spa/src/components/forms/product-form.tsx
+++ apps/admin-spa/src/components/forms/product-form.tsx
@@ -9,15 +9,6 @@ import { useApiForm } from "../../utils/useApiForm";
 import { Combobox } from "../combobox";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { ImageManager } from "../image-manager";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../ui/carousel";
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import { cn } from "../../lib/utils";
 
 type ProductCreateInputs = AdminInputs["catalog"]["product"]["createProduct"];
 
@@ -74,6 +65,7 @@ export function ProductForm({
       price: values.price,
       categoryId: values.category_id,
       attributes: values.attributes || [],
+      images: values.images || [],
       descriptions: languages.map((lang) => {
         const old = values.descriptions.find((d) => d.language_id === lang.id);
         return {
@@ -87,6 +79,7 @@ export function ProductForm({
       price: 0,
       categoryId: undefined as undefined | number,
       attributes: [],
+      images: [],
       descriptions: languages.map((lang) => {
         return {
           languageId: lang.id,
@@ -107,7 +100,8 @@ export function ProductForm({
     <form
       onSubmit={handleSubmit((data) => {
         clearErrors();
-        console.log(data);
+        console.log(data.images);
+
         // onSubmit(data);
       })}
       className="flex flex-col gap-8"
@@ -192,56 +186,16 @@ export function ProductForm({
             <CardContent>
               <ImageManager
                 enableSelect
-                limit={1}
-                // defaultSelected={values?.image_id ? [values.image_id] : []}
+                enableOrdering
+                limit={5}
+                defaultSelected={getValues("images")}
                 onSelectChange={(images) => {
-                  console.log({ images });
-                  // if (images.length > 0) {
-                  //   register("imageId").onChange({
-                  //     target: {
-                  //       name: "imageId",
-                  //       value: images[0].id,
-                  //     },
-                  //   });
-                  // }
+                  setValue(
+                    "images",
+                    images.map((i) => i.id),
+                  );
                 }}
               />
-
-              <Carousel>
-                <CarouselContent>
-                  {undefined?.map((file) => {
-                    const image = data?.find((i) => i.id === file);
-                    if (!image) {
-                      return null;
-                    }
-                    return (
-                      <CarouselItem className="xl:basis-1/3" key={image.id}>
-                        <AspectRatio
-                          ratio={4 / 4}
-                          className={cn(
-                            "relative rounded-md border-slate-200 border-2",
-                          )}
-                        >
-                          <div className="w-full h-full">
-                            <div
-                              className={cn(
-                                "absolute inset-0 invisible opacity-0 duration-200 transition-all bg-slate-400/50",
-                              )}
-                            />
-
-                            <img
-                              src={image.url}
-                              className="w-full h-full object-cover"
-                            />
-                          </div>
-                        </AspectRatio>
-                      </CarouselItem>
-                    );
-                  })}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-              </Carousel>
             </CardContent>
           </Card>
         </TabsContent>
