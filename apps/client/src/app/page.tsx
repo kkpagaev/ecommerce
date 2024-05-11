514aec8229b7df4484208e6c2eee6b0dc7fbc592
kkpagaev <kkpagaev@gmail.com>
2024-05-12 00:26:06 +0300

    add trpc router to next

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,7 +1,12 @@
+import { trpc } from "../utils/trpc";
+
 export default async function Home() {
+  const res = await trpc.admin.admin.listAdmins.query({ })
   return (
     <div>
-      foo
+    {res.map((a)=> {
+      return <div key={a.id}>{a.name}</div>
+    })}
     </div>
   );
 }
